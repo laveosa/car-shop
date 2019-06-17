25eb45097e5e74877e10833d16ed7df951ea6ac1
Nikolay Grygoryev <nikolay@barnyardcreative.com>
2019-06-18 01:45:05 +0300

    Updates

---
+++ precache-manifest.b5e066d02b900c39132966e9cdf4e953.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3fa4b3dea9dfb0858508d3d1a5630e5",
+    "url": "/car-shop/index.html"
+  },
+  {
+    "revision": "31d93ac89d83131e5c65",
+    "url": "/car-shop/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "41188771f2462c9ac962",
+    "url": "/car-shop/static/css/main.6dae60dd.chunk.css"
+  },
+  {
+    "revision": "31d93ac89d83131e5c65",
+    "url": "/car-shop/static/js/2.4a1ef0bf.chunk.js"
+  },
+  {
+    "revision": "41188771f2462c9ac962",
+    "url": "/car-shop/static/js/main.c4982f18.chunk.js"
+  },
+  {
+    "revision": "a716efe184209956a7b2",
+    "url": "/car-shop/static/js/runtime~main.68ae00d6.js"
+  },
+  {
+    "revision": "e8179884a14a4c2292f291e366d9642c",
+    "url": "/car-shop/static/media/car-key.e8179884.svg"
+  },
+  {
+    "revision": "415375be30cab2a4e9e0208063a4ccad",
+    "url": "/car-shop/static/media/car-multiple.415375be.svg"
+  },
+  {
+    "revision": "3b3b5dde91dd3581720cef2cd0092f6f",
+    "url": "/car-shop/static/media/car.3b3b5dde.svg"
+  }
+]);
